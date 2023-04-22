fdae36da4c1f9d9bf3351c5c233c23315938c89b
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-04-22 17:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-//your code here
+var btn = document.getElementById("btn");
+var respond = document.getElementById("respond");
 
+function ans() {
+  var no = Math.floor(Math.random() * 100);
+  alert("u have clciked ");
+  respond.textContent = no;
+}
+
+btn.addEventListener("click", ans);
