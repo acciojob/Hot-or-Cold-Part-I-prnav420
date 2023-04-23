5217d61b0a05262699afe29db1c8d86051555d9a
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-04-23 08:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 var btn = document.getElementById("btn");
 var not = document.getElementById("not");
 var respond = document.getElementById("respond");
-var guess = document.getElementById("guess").value;
-var diff = Number(guess) - Number(not.value);
 
-if (Math.abs(Number(guess)) < Number(not)) {
-  respond.textContent = "guess higher";
-} else if (Math.abs(Number(guess)) > Number(not)) {
-  respond.textContent = "guess lower";
-} else {
-  respond.textContent = "Getting hotter";
+function guess() {
+  var guessValue = Number(document.getElementById("guess").value);
+  var diff = guessValue - Number(not.value);
+
+  if (Math.abs(diff) < Math.abs(guessValue - Number(not.value))) {
+    respond.textContent = "Getting hotter";
+  } else if (guessValue < Number(not.value)) {
+    respond.textContent = "guess higher";
+  } else if (guessValue > Number(not.value)) {
+    respond.textContent = "guess lower";
+  } else {
+    respond.textContent = "You got it!";
+  }
 }
 
 function ans() {
   var no = Math.floor(Math.random() * 100);
-
   not.textContent = no;
 }
 
-btn.addEventListener("click", ans);
+btn.addEventListener("click", function() {
+  guess();
+  ans();
+});
